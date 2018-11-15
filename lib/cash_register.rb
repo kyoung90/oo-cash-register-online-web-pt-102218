9bd77ee5a88637ad2c104777a40c6371a8785615
Kenneth Young Castro <kenneth.young90@gmail.com>
2018-11-15 20:10:15 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,11 +1,11 @@
 class CashRegister
   attr_accessor :total, :discount, :last_transaction
   
-  @@items = []
   
   def initialize(discount = 0)
     @total = 0
     @discount = discount
+    @item = []
   end 
   
   def add_item(title, price, quantity = 1)
