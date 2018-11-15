d474b7023465129f57d761ed83586b030a423bab
Kenneth Young Castro <kenneth.young90@gmail.com>
2018-11-15 20:04:23 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,7 +11,7 @@ class CashRegister
   def add_item(title, price, quantity = 1)
     @total += price * quantity
     @@items << title
-    last_transaction = price
+    @last_transaction = price
   end 
   
   def apply_discount
